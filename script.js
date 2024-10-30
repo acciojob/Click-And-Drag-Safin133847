f8d13acf868c7f13d6e009d0c8bec26678e35777
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2024-10-30 19:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Your code here.
+ const itemsContainer = document.querySelector('.items');
+  let isDragging = false;
+  let startX;
+  let scrollLeft;
+
+  itemsContainer.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    startX = e.pageX - itemsContainer.offsetLeft;
+    scrollLeft = itemsContainer.scrollLeft;
+    itemsContainer.classList.add('active');
+  });
+
+  itemsContainer.addEventListener('mouseleave', () => {
+    isDragging = false;
+    itemsContainer.classList.remove('active');
+  });
+
+  itemsContainer.addEventListener('mouseup', () => {
+    isDragging = false;
+    itemsContainer.classList.remove('active');
+  });
+
+  itemsContainer.addEventListener('mousemove', (e) => {
+    if (!isDragging) return; // Stop the fn from running
+    e.preventDefault();
+    const x = e.pageX - itemsContainer.offsetLeft;
+    const walk = (x - startX) * 2; // The scroll-fast factor
+    itemsContainer.scrollLeft = scrollLeft - walk;
+  });
